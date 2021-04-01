7160299b968927d4a6dc03311aaea69591bb16a2
Mostafa Hazem <mostafa17_2@live.com>
2021-04-01 10:14:33 +0200

    added Celestial Objects model

---
+++ pluralsight-projects-AspNetCore-StarChartAPI-29dc253/StarChart/Models/CelestialObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarChart.Models
+{
+    public class CelestialObject
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public int? OrbitedObjectid { get; set; }
+
+        [NotMapped]
+        public List<CelestialObject> Sattelites { get; set; }
+
+        public TimeSpan OrbitalPeriod { get; set; }
+    }
+}
